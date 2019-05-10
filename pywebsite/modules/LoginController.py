8d1f8dd0a7ea161af562d5087e9d87be7e17ff13
Cedric Megnie <blackleeder@gmail.com>
2019-05-10 17:43:51 +0000

    completed & renamed login function to signIn added logout function

--- pywebsite/modules/LoginController.py
+++ pywebsite/modules/LoginController.py
@@ -1,6 +1,22 @@
-from modules import Model
+from modules import Model, auth
+from flask import session, url_for, redirect, Flask
 
-def login(_data) :
-  if Model.UserExists(_data) :
+# app = Flask(__name__)
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+def signIn(_data) :
+  _user = Model.UserExists(_data)
+  if _user :
+    auth.logInUser(_user)
     return True
   return False
+
+def logout() :
+  session.pop('_userId',None)
+  session.pop('_username',None)
+  session.pop('_role',None)
+  return True
+
+# for texting 
+# _data = {'email':'johndoe@gmail.com', 'password':'123456'}
+# with app.test_request_context():
+#   login(_data)
