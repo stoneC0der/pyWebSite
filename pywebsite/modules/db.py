7be92dec5526426276f40b7ddaf4f26699ece09c
Cedric Megnie <blackleeder@gmail.com>
2019-05-10 17:46:15 +0000

    added a factory method to get record as dictionary instead of tuple & some refactoring

--- pywebsite/modules/db.py
+++ pywebsite/modules/db.py
@@ -13,6 +13,10 @@ from sqlite3 import Error
 
 # con.execute("INSERT INTO tbl_users (`name`,`email`,`password`,`city`,`role`) VALUES('johndoe','johndoe@gmail.com','123456','Accra','1')")
 
+''''
+Connect to the database
+Return tuple _con
+'''
 def getDB(_db_file) :
   """ create a database connection to the SQLite database
   specified by the db_file
@@ -20,50 +24,73 @@ def getDB(_db_file) :
   :return: Connection object or None
   """
   try :
-    con = sql.connect(_db_file)
+    _con = sql.connect(_db_file)
     print ("Opened database successfully")
-    return con
-  except Error as e :
-    print(e)
+    return _con
+  except Error as _e :
+    print(_e)
   return None
+
+# Define a raw factory function to convert tuple as dictionary
+def dict_factory(_cur,_row) :
+  _dic = {}
+  for _index, _col in enumerate(_cur.description) :
+    _dic[_col[0]] = _row[_index]
+  return _dic
+
 # TODO:  Change the way user login, findUserByEmail should only be use to check if email is taken using ajax, so no need to retrieve id for login we'll just use email & passowrd
 # data = {'email':'johndoe@gmail.com','password':'123456'}
-def findUser(con,data) :
-  cur = con.cursor()
-  _email = data['email']
+
+''''
+Check if the user info exists, if yes return True, False otherwise
+Used for log in the user
+return Boolean
+'''
+def findUser(_con,_data) :
+  _con.row_factory = dict_factory
+  _cur = _con.cursor()
+  _email = _data['email']
   # _hashed_password = password_hash(data['password'])
-  _password = data['password']
+  _password = _data['password']
   # print(_password)
-  _id = findUserByEmail(con,_email)
+  _id = findUserByEmail(_con,_email)
   
   if _id :
-    cur.execute('SELECT * FROM tbl_users WHERE `user_id`=? AND password=?', (_id[0],_password))
-    _user = cur.fetchall()
+    _cur.execute('SELECT * FROM tbl_users WHERE `user_id`=? AND password=?', (_id,_password))
+    _user = _cur.fetchall()
     if _user :
       print('User exists!')
-      return True
+      for _u in _user :
+        return _u
     else :
       print('Not Does not exists!')
       return False
 
-def findUserByEmail(con,_email) :
-  cur = con.cursor()
+'''
+Get userId by email
+return Int _id the username
+Used to check if email is already taken via ajax
+'''
+def findUserByEmail(_con,_email) :
+  _con.row_factory = dict_factory
+  _cur = _con.cursor()
   
   if _email :
     # sqlite3.ProgrammingError: Incorrect number of bindings supplied. The current statement uses 1, and there are 17 supplied.
     # You need to pass in a sequence, but you forgot the comma to make your parameters a tuple:
-    cur.execute('SELECT user_id FROM tbl_users WHERE email=?',(_email,))
-    _id = cur.fetchall()
+    _cur.execute('SELECT user_id FROM tbl_users WHERE email=?',(_email,))
+    _id = _cur.fetchall()
     if _id :
       for _user_id in _id :
-        # print(_user_id[0])
-        return _user_id
+        # print(_user_id['user_id'])
+        return _user_id['user_id']
     else :
       return 0
   return 'Email not found'
 
 # exit()
-# findUser(con,data)
+# _data = {'email':'johndoe@gmail.com', 'password':'123456'}
+# findUser(getDB('pyweb_db.sqlite'),_data)
 
 '''
 Where you make your connection to the database add the following.
